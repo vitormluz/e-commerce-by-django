30fff299c4502e7f631cff97ba1ad7114cfdbedf
vitormluz <vitormdluzz2003@outlook.com>
2022-09-01 15:24:22 -0300

    templates_commit(detalhes)

--- produto/views.py
+++ produto/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from . import models
 
 class ListaProdutos(ListView):
@@ -11,9 +12,11 @@ class ListaProdutos(ListView):
     paginate_by = 10
 
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Produto: DetalheProduto')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
