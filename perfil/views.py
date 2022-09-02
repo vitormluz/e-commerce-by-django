93107682a0316845a1f8154730c342cf16f490b8
vitormluz <vitormdluzz2003@outlook.com>
2022-09-02 15:02:11 -0300

    cadastro2

--- perfil/views.py
+++ perfil/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login as login_
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from . import models
@@ -23,7 +24,7 @@ class BasePerfil(View):
             self.perfil = models.Perfil.objects.filter(usuario=self.request.user).first()
             self.contexto = {
                 'userform': forms.UserForm(data=self.request.POST or None, usuario=self.request.user, instance=self.request.user),
-                'perfilform': forms.PerfilForm(data=self.request.POST or None),
+                'perfilform': forms.PerfilForm(data=self.request.POST or None, instance=self.perfil),
             }
         else:
             self.contexto = {
@@ -33,6 +34,9 @@ class BasePerfil(View):
 
         self.userform = self.contexto['userform']
         self.perfilform = self.contexto['perfilform']
+
+        if self.request.user.is_authenticated:
+            self.template_name = 'perfil/atualizar.html'
 
         self.renderizar = render(self.request, self.template_name, self.contexto)
 
@@ -66,6 +70,16 @@ class Criar(BasePerfil):
             usuario.last_name = last_name
             usuario.save()
 
+            if not self.perfil:
+                self.perfilform.cleaned_data['usuario'] = usuario
+                print(self.perfilform.cleaned_data)
+                perfil = models.Perfil(**self.perfilform.cleaned_data)
+                perfil.save()
+            else:
+                self.perfilform.save(commit=False)
+                perfil.usuario = usuario
+                perfil.save()
+
         else:
             usuario = self.userform.save(commit=False)
             usuario.set_password(password)
@@ -74,6 +88,12 @@ class Criar(BasePerfil):
             perfil = self.perfilform.save(commit=False)
             perfil.usuario = usuario
             perfil.save()
+
+        if password:
+            autentica = authenticate(self.request, username=usuario, password=password)
+
+            if autentica:
+                login_(self.request, user=usuario)
 
         self.request.session['carrinho'] = self.carrinho
         self.request.session.save()
